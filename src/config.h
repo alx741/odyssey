7eabd01af15a2ab0e9453c7f7797bb8380cc2af1
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-06-18 09:31:49 -0500

    Bump autotools version

--- src/config.h
+++ src/config.h
@@ -1,139 +1,140 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
 /* src/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to build the Direct I/O driver */
-#undef ENABLE_DIRECTIO
+#define ENABLE_DIRECTIO /**/
 
 /* Define to build the LinuxPPDev driver */
-#undef ENABLE_LINUX_PPDEV
+#define ENABLE_LINUX_PPDEV /**/
 
 /* Define to 1 if you have the <cctype> header file. */
-#undef HAVE_CCTYPE
+#define HAVE_CCTYPE 1
 
 /* Define to 1 if you have the <cerrno> header file. */
-#undef HAVE_CERRNO
+#define HAVE_CERRNO 1
 
 /* Define to 1 if you have the <csignal> header file. */
-#undef HAVE_CSIGNAL
+#define HAVE_CSIGNAL 1
 
 /* Define to 1 if you have the <cstdio> header file. */
-#undef HAVE_CSTDIO
+#define HAVE_CSTDIO 1
 
 /* Define to 1 if you have the <cstdlib> header file. */
-#undef HAVE_CSTDLIB
+#define HAVE_CSTDLIB 1
 
 /* Define to 1 if you have the <cstring> header file. */
-#undef HAVE_CSTRING
+#define HAVE_CSTRING 1
 
 /* Define to 1 if you have the <errno.h> header file. */
-#undef HAVE_ERRNO_H
+#define HAVE_ERRNO_H 1
 
 /* Define to 1 if you have the <exception> header file. */
-#undef HAVE_EXCEPTION
+#define HAVE_EXCEPTION 1
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#undef HAVE_FCNTL_H
+#define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the <getopt.h> header file. */
-#undef HAVE_GETOPT_H
+#define HAVE_GETOPT_H 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <iterator> header file. */
-#undef HAVE_ITERATOR
+#define HAVE_ITERATOR 1
 
 /* Define to 1 if you have the <linux/parport.h> header file. */
-#undef HAVE_LINUX_PARPORT_H
+#define HAVE_LINUX_PARPORT_H 1
 
 /* Define to 1 if you have the <linux/ppdev.h> header file. */
-#undef HAVE_LINUX_PPDEV_H
+#define HAVE_LINUX_PPDEV_H 1
 
 /* Define to 1 if you have the <map> header file. */
-#undef HAVE_MAP
+#define HAVE_MAP 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <new> header file. */
-#undef HAVE_NEW
+#define HAVE_NEW 1
 
 /* Define to 1 if you have the `select' function. */
-#undef HAVE_SELECT
+#define HAVE_SELECT 1
 
 /* Define to 1 if you have the <signal.h> header file. */
-#undef HAVE_SIGNAL_H
+#define HAVE_SIGNAL_H 1
 
 /* Define to 1 if you have the `snprintf' function. */
-#undef HAVE_SNPRINTF
+#define HAVE_SNPRINTF 1
 
 /* Define to 1 if you have the <stdexcept> header file. */
-#undef HAVE_STDEXCEPT
+#define HAVE_STDEXCEPT 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdio.h> header file. */
-#undef HAVE_STDIO_H
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <string> header file. */
-#undef HAVE_STRING
+#define HAVE_STRING 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/ioctl.h> header file. */
-#undef HAVE_SYS_IOCTL_H
+#define HAVE_SYS_IOCTL_H 1
 
 /* Define to 1 if you have the <sys/io.h> header file. */
-#undef HAVE_SYS_IO_H
+#define HAVE_SYS_IO_H 1
 
 /* Define to 1 if you have the <sys/select.h> header file. */
-#undef HAVE_SYS_SELECT_H
+#define HAVE_SYS_SELECT_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/time.h> header file. */
-#undef HAVE_SYS_TIME_H
+#define HAVE_SYS_TIME_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to 1 if you have the <vector> header file. */
-#undef HAVE_VECTOR
+#define HAVE_VECTOR 1
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "odyssey"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "vasco-project@lists.gforge.enseeiht.fr"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "odyssey"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "odyssey 0.6"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "odyssey"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "0.6"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "0.6"
